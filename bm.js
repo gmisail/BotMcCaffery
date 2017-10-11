68a3162d020e3ac3e166ef31df158aab6123751a
Graham Misail <unconfigured@null.spigotmc.org>
2017-10-11 19:41:13 -0400

    im dumb

--- bm.js
+++ bm.js
@@ -7,23 +7,23 @@ function on_message_recieved(e)
     var message = e.content.toLowerCase()
     if(message == "never gonna")
     {
-        e.sendMessage("give you up, never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, never gonna say goodbye, Never gonna tell a lie and hurt you")
+        e.reply("give you up, never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, never gonna say goodbye, Never gonna tell a lie and hurt you")
     }
     else if(message == "hey now")
     {
-        e.sendMessage("https://www.youtube.com/watch?v=L_jWHffIx5E")
+        e.reply("https://www.youtube.com/watch?v=L_jWHffIx5E")
     }
     else if(message == "rick")
     {
-        e.sendMessage("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        e.reply("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     }
     else if(message == "$400")
     {
-        e.sendMessage("http://www.dryships.com/")
+        e.reply("http://www.dryships.com/")
     }
     else if(message == "where is my money?")
     {
-        e.sendMessage(":flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: ")
+        e.reply(":flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: ")
     }
 }
 
