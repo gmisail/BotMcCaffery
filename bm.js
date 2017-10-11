8b635bedff1a19a52edae7a72cdcf40aa3a3f105
Graham Misail <unconfigured@null.spigotmc.org>
2017-10-11 19:38:27 -0400

    test?

--- bm.js
+++ bm.js
@@ -1,33 +1,32 @@
-var Discord = require('discordie')
+var Discord = require('discord.js')
 
-const Events = Discord.Events
-const Client = new Discord()
+const Client = new Discord.Client()
 
 function on_message_recieved(e)
 {
-    var message = e.message.content.toLowerCase()
+    var message = e.content.toLowerCase()
     if(message == "never gonna")
     {
-        e.message.channel.sendMessage("give you up, never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, never gonna say goodbye, Never gonna tell a lie and hurt you")
+        e.sendMessage("give you up, never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, never gonna say goodbye, Never gonna tell a lie and hurt you")
     }
     else if(message == "hey now")
     {
-        e.message.channel.sendMessage("https://www.youtube.com/watch?v=L_jWHffIx5E")
+        e.sendMessage("https://www.youtube.com/watch?v=L_jWHffIx5E")
     }
     else if(message == "rick")
     {
-        e.message.channel.sendMessage("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        e.sendMessage("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     }
     else if(message == "$400")
     {
-        e.message.channel.sendMessage("http://www.dryships.com/")
+        e.sendMessage("http://www.dryships.com/")
     }
     else if(message == "where is my money?")
     {
-        e.message.channel.sendMessage(":flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: ")
+        e.sendMessage(":flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: :flag_gr: ")
     }
 }
 
-Client.Dispatcher.on(Events.MESSAGE_CREATE, on_message_recieved)
+Client.on('message', on_message_recieved)
 
 Client.login(process.env.BOT_TOKEN)
