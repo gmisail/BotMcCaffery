2b95c23b0bdd7a594d11ddea7020ee5d029ec617
Graham Misail <unconfigured@null.spigotmc.org>
2017-10-11 21:22:00 -0400

    tts again?

--- bm.js
+++ bm.js
@@ -7,7 +7,7 @@ function on_message_recieved(e)
     var message = e.content.toLowerCase()
     if(message == "never gonna")
     {
-        e.tts("give you up, never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, never gonna say goodbye, Never gonna tell a lie and hurt you")
+        e.reply("give you up, never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, never gonna say goodbye, Never gonna tell a lie and hurt you", {tts: true})
     }
     else if(message == "hey now")
     {
