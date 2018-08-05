c21742fa0c7a79cf3038f71ba366723ced89e2cc
Graham Misail <gmisail64@gmail.com>
2018-08-05 11:42:54 -0400

    Again

--- bm.js
+++ bm.js
@@ -1,7 +1,7 @@
 const bot = require('./bot/bot');
 const help = require('./bot/commands/help');
 const d20 = require('./bot/commands/d20');
-const joke = require('../bot/joke');
+const joke = require('../bot/commands/joke');
 
 bot.start([
     help, d20
