46143d61d67860cb5adfafe397423113b18a858a
Graham Misail <gmisail64@gmail.com>
2018-08-05 11:44:02 -0400

    Did I forget how to require?

--- bm.js
+++ bm.js
@@ -1,8 +1,8 @@
 const bot = require('./bot/bot');
 const help = require('./bot/commands/help');
 const d20 = require('./bot/commands/d20');
-const joke = require('../bot/commands/joke');
+const joke = require('./bot/commands/joke');
 
 bot.start([
-    help, d20
+    help, d20, joke
 ]);  
